b7c27d28e12831341795ff4a94791c543fc92d53
jalovisko <letovnn@gmail.com>
2021-04-10 00:42:46 -0400

    Now can read XLSX files with words

--- ringuo.py
+++ ringuo.py
@@ -3,27 +3,13 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'This application searches transcriptions from the Oxford dictionary')
-    parser.add_argument('-w', dest = 'word',
-            help = 'The word of interest')
-    parser.add_argument('-i', dest = 'input_filename',
-            help = 'Input Excel filename with words of interest')
-    cli_args = parser.parse_args()
-    if cli_args.word is not None:
-        word = str(cli_args.word)
-    elif cli_args.input_filename is not None:
-        dfs = pd.read_excel(input_filename, sheet_name = "Sheet1")
-        word = dfs.iloc[i, 0] # every entry from the 1st column
-    else:
-        raise ValueError('No input is provided')
-
+def setup_chrome():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=%s" % '800,600')
+    return webdriver.Chrome(options = chrome_options)
 
-    # Using Chrome to access web
-    driver = webdriver.Chrome(options=chrome_options)
+def get_transcription(driver, word):
     # Open the website
     driver.get('https://www.oxfordlearnersdictionaries.com/definition/english/')
     # Select the search box
@@ -40,3 +26,27 @@ if __name__ == '__main__':
         print(transcriptions.text)
     else:
         print(transcriptions[0].text)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'This application searches transcriptions from the Oxford dictionary')
+    parser.add_argument('-w', dest = 'word',
+            help = 'The word of interest')
+    parser.add_argument('-i', dest = 'input_filename',
+            help = 'Input Excel filename with words of interest')
+    cli_args = parser.parse_args()
+    word = cli_args.word
+    input_filename = cli_args.input_filename
+    # Using Chrome to access web
+    driver = setup_chrome()
+    if word is not None:
+        get_transcription(driver, word)    
+    elif input_filename is not None:
+        dfs = pd.read_excel(input_filename, sheet_name = "Sheet1")
+        for i in range(len(dfs)):
+            word = dfs.iloc[i, 0] # every entry from the 1st column
+            get_transcription(driver, word)
+    else:
+        raise ValueError('No input is provided')
+
+                                                          
+        
